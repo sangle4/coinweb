29155ad6aecc9541a1d29baea8cbe481f51d97ac
sangle4 <sangle4@naver.com>
2019-09-09 23:53:00 +0900

    add find controller and function

--- WebContent/js/userLogin.js
+++ WebContent/js/userLogin.js
@@ -14,10 +14,26 @@ function findIDBT() {
 	if(document.getElementById("username").value == ""){
 		alert("가입한 사용자 이름을 입력하세요.");
 	}
+	else{
+		checkNAMEinDB(document.getElementById("username").value);
+	}
 }
 
 function findPWB() {
 	if(document.getElementById("userid").value == ""){
 		alert("가입한 사용자 아이디를 입력하세요.");
 	}
+	else{
+		checkIDinDB(document.getElementById("userid").value);
+	}
+}
+
+///////
+
+function checkNAMEinDB(userName) {
+	
+}
+
+function checkIDinDB(userID) {
+	
 }
